d4fe7753909400e1b1347a5eb8c103a824f85f1c
Minami Tomonobu <minami@edanoue.com>
2024-03-08 10:05:22 +0900

    :white_check_mark: Add Debounce test and update other tests
    A new test for the Debounce function has been created and added. The TS_TakeWhile and TS_Merge tests were also updated for readability and efficiency, respectively. Additionally, a new reference to Microsoft.Bcl.TimeProvider.dll was included in the Edanoue.Rx.EditorTests assembly definition.
    
    Signed-off-by: Minami Tomonobu <minami@edanoue.com>

---
+++ Tests/Editor/Operator/TS_Debounce.cs
@@ -0,0 +1,28 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+#nullable enable
+#if SUPPORT_TIME_PROVIDER
+using System;
+using NUnit.Framework;
+
+namespace Edanoue.Rx
+{
+    public sealed class TS_Debounce
+    {
+        [Test]
+        public void Debounce()
+        {
+            var publisher = new Subject<int>();
+            var result = 0;
+            using var list = publisher.Debounce(TimeSpan.FromSeconds(3)).Subscribe(x => result = x);
+
+            publisher.OnNext(1);
+            Assert.That(result, Is.EqualTo(0));
+            publisher.OnNext(10);
+            Assert.That(result, Is.EqualTo(0));
+            publisher.OnNext(100);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
+#endif
