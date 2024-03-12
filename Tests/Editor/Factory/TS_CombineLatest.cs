6814ebe99454ad15c9e69b774662773339c50ad9
Minami Tomonobu <minami@edanoue.com>
2024-03-12 10:25:54 +0900

    :sparkles: Added CombineLatest operator
    This update adds a new CombineLatest operator to the Reactive Extensions package. The operator observes multiple Observables and produces a value whenever any Observable from the group emits a new value. Detailed documentation is added to README.md as well.
    
    Signed-off-by: Minami Tomonobu <minami@edanoue.com>

---
+++ Tests/Editor/Factory/TS_CombineLatest.cs
@@ -0,0 +1,53 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+using NUnit.Framework;
+
+namespace Edanoue.Rx
+{
+    public sealed class TS_CombineLatestFactory
+    {
+        [Test]
+        [Category("Normal")]
+        public void Test()
+        {
+            var source1 = new Subject<int>();
+            var source2 = new Subject<int>();
+            int[] result = default;
+            var isCompleted = false;
+
+            using var subscription = Observable
+                .CombineLatest(source1, source2)
+                .Subscribe(x => result = x,
+                    _ => isCompleted = true
+                );
+
+            source1.OnNext(1);
+            Assert.That(result, Is.EqualTo(default(int[])));
+
+            source1.OnNext(2);
+            Assert.That(result, Is.EqualTo(default(int[])));
+
+            source2.OnNext(1);
+            Assert.That(result, Is.EqualTo(new[] { 2, 1 }));
+
+            source1.OnNext(3);
+            Assert.That(result, Is.EqualTo(new[] { 3, 1 }));
+
+            source2.OnNext(2);
+            Assert.That(result, Is.EqualTo(new[] { 3, 2 }));
+
+            source2.OnNext(3);
+            Assert.That(result, Is.EqualTo(new[] { 3, 3 }));
+
+            source1.OnCompleted();
+            Assert.That(isCompleted, Is.False);
+
+            source1.OnNext(4);
+            source2.OnNext(4);
+            Assert.That(result, Is.EqualTo(new[] { 3, 4 }));
+
+            source2.OnCompleted();
+            Assert.That(isCompleted, Is.True);
+        }
+    }
+}
---
+++ Tests/Editor/Operator/TS_CombineLatest.cs
@@ -0,0 +1,54 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+using System;
+using NUnit.Framework;
+
+namespace Edanoue.Rx
+{
+    public sealed class TS_CombineLatest
+    {
+        [Test]
+        [Category("Normal")]
+        public void Test()
+        {
+            var source1 = new Subject<int>();
+            var source2 = new Subject<string>();
+            (int, string) result = default;
+            var isCompleted = false;
+
+            using var subscription = source1
+                .CombineLatest(source2, ValueTuple.Create)
+                .Subscribe(x => result = x,
+                    _ => isCompleted = true
+                );
+
+            source1.OnNext(1);
+            Assert.That(result, Is.EqualTo(default((int, string))));
+
+            source1.OnNext(2);
+            Assert.That(result, Is.EqualTo(default((int, string))));
+
+            source2.OnNext("a");
+            Assert.That(result, Is.EqualTo((2, "a")));
+
+            source1.OnNext(3);
+            Assert.That(result, Is.EqualTo((3, "a")));
+
+            source2.OnNext("b");
+            Assert.That(result, Is.EqualTo((3, "b")));
+
+            source2.OnNext("c");
+            Assert.That(result, Is.EqualTo((3, "c")));
+
+            source1.OnCompleted();
+            Assert.That(isCompleted, Is.False);
+
+            source1.OnNext(4);
+            source2.OnNext("d");
+            Assert.That(result, Is.EqualTo((3, "d")));
+
+            source2.OnCompleted();
+            Assert.That(isCompleted, Is.True);
+        }
+    }
+}
