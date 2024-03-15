aba27b21bf64bbb7b2c1394ee81161b9b538238b
Minami Tomonobu <minami@edanoue.com>
2024-03-15 10:32:05 +0900

    :bug: Revert Distinct
    Signed-off-by: Minami Tomonobu <minami@edanoue.com>

---
+++ Runtime/Operators/Distinct.cs
@@ -0,0 +1,69 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+namespace Edanoue.Rx
+{
+    public static partial class ObservableExtensions
+    {
+        public static Observable<T> Distinct<T>(this Observable<T> source)
+        {
+            return Distinct(source, EqualityComparer<T>.Default);
+        }
+
+        public static Observable<T> Distinct<T>(this Observable<T> source, IEqualityComparer<T> comparer)
+        {
+            return new Distinct<T>(source, comparer);
+        }
+    }
+
+    internal sealed class Distinct<T> : Observable<T>
+    {
+        private readonly IEqualityComparer<T> _comparer;
+        private readonly Observable<T>        _source;
+
+        public Distinct(Observable<T> source, IEqualityComparer<T> comparer)
+        {
+            _source = source;
+            _comparer = comparer;
+        }
+
+        protected override IDisposable SubscribeCore(Observer<T> observer)
+        {
+            return _source.Subscribe(new _Distinct(observer, _comparer));
+        }
+
+        private sealed class _Distinct : Observer<T>
+        {
+            private readonly Observer<T> _observer;
+            private readonly HashSet<T>  _set;
+
+            public _Distinct(Observer<T> observer, IEqualityComparer<T> comparer)
+            {
+                _observer = observer;
+                _set = new HashSet<T>(comparer);
+            }
+
+            protected override void OnNextCore(T value)
+            {
+                if (_set.Add(value))
+                {
+                    _observer.OnNext(value);
+                }
+            }
+
+            protected override void OnErrorResumeCore(Exception error)
+            {
+                _observer.OnErrorResume(error);
+            }
+
+            protected override void OnCompletedCore(Result result)
+            {
+                _observer.OnCompleted(result);
+            }
+        }
+    }
+}
---
+++ Runtime/Operators/DistinctBy.cs
@@ -0,0 +1,76 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+namespace Edanoue.Rx
+{
+    public static partial class ObservableExtensions
+    {
+        public static Observable<TSource> DistinctBy<TSource, TKey>(this Observable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return DistinctBy(source, keySelector, EqualityComparer<TKey>.Default);
+        }
+
+        public static Observable<TSource> DistinctBy<TSource, TKey>(this Observable<TSource> source,
+            Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+        {
+            return new DistinctBy<TSource, TKey>(source, keySelector, comparer);
+        }
+    }
+
+    internal sealed class DistinctBy<T, TKey> : Observable<T>
+    {
+        private readonly IEqualityComparer<TKey> _comparer;
+        private readonly Func<T, TKey>           _keySelector;
+        private readonly Observable<T>           _source;
+
+        public DistinctBy(Observable<T> source, Func<T, TKey> keySelector, IEqualityComparer<TKey> comparer)
+        {
+            _source = source;
+            _keySelector = keySelector;
+            _comparer = comparer;
+        }
+
+        protected override IDisposable SubscribeCore(Observer<T> observer)
+        {
+            return _source.Subscribe(new _DistinctBy(observer, _keySelector, _comparer));
+        }
+
+        private sealed class _DistinctBy : Observer<T>
+        {
+            private readonly Func<T, TKey> _keySelector;
+            private readonly Observer<T>   _observer;
+            private readonly HashSet<TKey> _set;
+
+            public _DistinctBy(Observer<T> observer, Func<T, TKey> keySelector, IEqualityComparer<TKey> comparer)
+            {
+                _observer = observer;
+                _keySelector = keySelector;
+                _set = new HashSet<TKey>(comparer);
+            }
+
+            protected override void OnNextCore(T value)
+            {
+                var key = _keySelector(value);
+                if (_set.Add(key))
+                {
+                    _observer.OnNext(value);
+                }
+            }
+
+            protected override void OnErrorResumeCore(Exception error)
+            {
+                _observer.OnErrorResume(error);
+            }
+
+            protected override void OnCompletedCore(Result result)
+            {
+                _observer.OnCompleted(result);
+            }
+        }
+    }
+}
